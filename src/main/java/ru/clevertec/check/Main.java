849991470fd24aff20bb4264fb338517acbace33
Us10n <example@example.com>
2023-12-28 18:47:44 +0300

    Add some code that will fail checkstyle check

---
+++ src/main/java/ru/clevertec/check/Main.java
@@ -0,0 +1,13 @@
+package ru.clevertec.check;
+
+public class Main {
+    private static final String invalidConstantName = "Invalid constant";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void creatingSuperPuperLongMethodNameSoCheckStyleWillThrowErrorCreatingSuperPuperLongMethodNameSoCheckStyleWillThrowErrorcreatingSuperPuperLongMethodNameSoCheckStyleWillThrowError(){
+        System.out.println("Long method");
+    }
+}
